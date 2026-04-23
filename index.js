2c4725f7c7b6051ef5c56bd0783d8317e31204b3
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-23 15:06:04 +0500

    1. added midleware support to individual routes.

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const MyLib = require('./lib/myLib');
 
 const server = new MyLib();
 
-server.get("/hello/world", (req, res) => {
-    console.log("Hello from the get endpoint")
+// TODO: add middlewars
+// TODO: compression 
 
 
+
+function helloWorld(req, res, next) {
+    console.log("Hello world from GET /hello endpoint middleware");
+
+    // next();
+}
+server.get("/hello", helloWorld, (req, res) => {
+    console.log("Hello from the get endpoint callback")
+
+    res.status(200).json({ message: "Success" })
 });
 
-server.post("/post", (req, res) => {
-    console.log("Hello from post endpoint");
-    res.status(200).json({ message: "Hello world" });
-});
+// server.post("/post", (req, res) => {
+//     console.log("Hello from post endpoint");
+//     res.status(200).json({ message: "Hello world" });
+// });
 
-server.patch('/patching/:name', (req, res) => {
-    const name = req.params.name;
-});
+// server.patch('/patching/:name', (req, res) => {
+//     const name = req.params.name;
+// });
 
-server.delete("/post", (req, res) => {
+// server.delete("/post", (req, res) => {
 
-})
+// })
 
 server.listen(3000, () => {
     console.log(`Server listening on http://localhost:${3000}`)
