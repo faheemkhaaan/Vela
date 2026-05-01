250a718418a42b6ebc88d1cc9c595aad082d7e0d
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-05-01 22:52:01 +0500

    1. handling route params. 2. there is a bug that i will fix later.

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,7 +4,9 @@ const Router = require('../lib/router')
 
 const router = new Router();
 
-router.get("/testing-router", (req, res) => {
+router.get("/testing-router/:userId/posts/:postId", (req, res) => {
+    const { userId, postId } = req.params;
+    console.log(userId, postId)
     res.status(200).json({ message: "Hello from test-router" })
 })
 module.exports = router
